d47666ea10c0e8300ae8618a3d1710e72f302226
fballou <114373809+fballou@users.noreply.github.com>
2024-05-01 15:38:17 -0700

    Adding Image

--- src/about.js
+++ src/about.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import deer from './deer.jpeg';
 import './App.css';
 
 import React from 'react';
@@ -22,6 +23,15 @@ function About() {
                 </Nav>
                 </Container>
             </Navbar>
+            <div class="who-i-am">
+              <p class="my-career">Software Engineer & UX Designer</p>
+              <p class="my-info">
+                Hello, my name is Fadi Ballou. I'm a junior at UCSD studing Cognitive Science w/Design & Interaction and Computer Science. I've worked in the UX Design industry and have had 2 internships in Software Engineering. I aspire to use my skills to become a Full Stack Developer that's capable of designing an optimal front end and creating a functional backend as well.
+              </p>
+              <button type="button" class="btn btn-light contact-me">Contact Me</button>
+            </div>
+            {/* <img src="/src/images/deer.jpeg" alt="Deer why don't you work?" className="deer-image" /> */}
+            <img src={deer} alt="deer image" className="deer-image" />
         </div>
     );
 
